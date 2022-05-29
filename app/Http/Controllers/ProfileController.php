1b1b7b760694abce0a4a3329a8b0fcb31759a8b4
Felix Kiptoo <felixkpt@gmail.com>
2022-05-29 08:54:37 +0300

    cummulative updates on frontend

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\Models\Review;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -18,9 +20,17 @@ class ProfileController extends Controller
             return redirect()->back()->with('danger', 'User not found');
         }
         $description = 'Profile details & Reviews by '.$user->name;
-        $reviews = Review::where('user_id', $user->id)->whereHas('post', function($q) use($user) {
-            $q->where('reviews.user_id', $user->id);
-        })->orderBy('updated_at', 'desc')->paginate(10);
+        // user can view their review while it awaits moderation
+        if (Auth::user() && Auth::user()->id == $user->id) {
+            $reviews = Review::where([['user_id', $user->id]])->whereHas('post', function($q) use($user) {
+                $q->where('reviews.user_id', $user->id);
+            })->orderBy('updated_at', 'desc')->paginate(10);
+        }else {
+            $reviews = Review::where([['published', 'published'], ['user_id', $user->id]])->whereHas('post', function($q) use($user) {
+                $q->where('reviews.user_id', $user->id);
+            })->orderBy('updated_at', 'desc')->paginate(10);
+    
+        }
         
         $title = 'Reviews by '.$user->name.' ('.$reviews->total().')';
         return view('profile.show', ['title' => $title, 'description' => $description, 'user' => $user, 'reviews' => $reviews]);
