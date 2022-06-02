61a09a4d4d839f391a28617d02c77add63582cef
Felix Kiptoo <felixkpt@gmail.com>
2022-06-02 13:35:06 +0300

    Modified content OK

--- resources/views/posts/components/notify-unapproved.blade.php
+++ resources/views/posts/components/notify-unapproved.blade.php
@@ -1,11 +1,13 @@
-<div class="w-full mt-3">
-    <div class="flex flex-wrap w-full">
-        <div class="w-full">
-            <div class="alert alert-info flex flex-wrap w-full justify-between">
-                <div class="my-auto">
-                    <strong>Info: </strong>Company review post is awaiting moderation. In the mean time you can edit where necessary.
+<div class="col-12 mt-3">
+    <div class="row">
+        <div class="col-12">
+            <div class="alert alert-info">
+                <div class="d-flex justify-content-between p-2">
+                    <div>
+                        <strong>Info: </strong>Your review is awaiting moderation. In the mean time you can edit where necessary.
+                    </div>
+                    <a class="btn btn-sm main-outline-btn pt-2" href="{{ url('write-a-review/'.$post->id) }}">Edit my review</a>
                 </div>
-                <a class="btn btn-sm main-outline-btn pt-2" href="{{ url('write-a-review/'.$post->id) }}">Edit my review</a>
             </div>
         </div>
     </div>
