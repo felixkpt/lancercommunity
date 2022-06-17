79ac62a15c6e8265d2b8f1afd4da2005e3ab8063
Felix Kiptoo <felixkpt@gmail.com>
2022-06-17 23:41:48 +0300

    admin create post setup description

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -9,6 +9,7 @@ use App\Http\Requests\Auth\LoginRequest;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Settings\SiteInfo;
 
 class AuthenticatedSessionController extends Controller
 {
