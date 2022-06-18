dd3b40b7914b0bc9b44e88e4b0bca2314a71eb8e
Felix Kiptoo <felixkpt@gmail.com>
2022-06-18 09:06:11 +0300

    admin create post setup description

--- config/settings.php
+++ config/settings.php
@@ -2,8 +2,8 @@
 return [
     'siteInfo' => [
         'name' => 'LancerCommunity',
-    'title' => 'Detailed websites review',
-    'description' => 'Detailed websites review, get the right company and freelancer for your business',
+    'title' => 'Read or leave unbiased reviews of top freelance services',
+    'description' => 'Get the right company and freelancer for your business. Start by reading unbiased reviews of freelancing services. You can also leave a review to help other users',
     ],
     'reviews' => [
         'stars' => 5,
