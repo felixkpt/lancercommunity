a65ef5b2b309a524965a4c027176e95d3970d7fa
Felix Kiptoo <felixkpt@gmail.com>
2022-06-26 13:44:58 +0300

    Finished revisions specified

--- resources/views/posts/components/featured-posts.blade.php
+++ resources/views/posts/components/featured-posts.blade.php
@@ -8,14 +8,14 @@
 <div class="row flex-column align-items-start justify-content-between">
     <div class="col-12">
         <div class="row w-100">
-            <div class="pl-1 rounded col-4 col-md-12 col-xl-4 nx-auto overflow-hidden">
+            <div class="pl-1 rounded col-4 col-sm-3 col-md-12 col-xl-4 nx-auto overflow-hidden">
                 <a href="{{ url('company/'.$item->slug) }}" class="block w-100">
                     <div class="thumb-image">
                         <img class="rounded w-100 h-100" src="{{ isset($item->image) ? $item->image : asset('images/default-company.png') }}" alt="">
                     </div>
                 </a>
             </div>
-            <div class="pl-1 col-8 col-md-12 col-xl-8 text-left text-xlg-center"><a style="font-size: 16px" href="{{ url('company/'.$item->slug) }}">{{ $item->company_name }}</a></div>
+            <div class="pl-1 col-8 col-sm-9 col-md-12 col-xl-8 text-left text-xlg-center"><a style="font-size: 16px" href="{{ url('company/'.$item->slug) }}">{{ $item->company_name }}</a></div>
         </div>
         <div class="row my-2">
             <a class="text-dark" href="{{ url('company/'.$item->slug) }}">
