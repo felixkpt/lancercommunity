98c2c41a1d608e40b6d2b145f49a63560f75984b
Felix Kiptoo <felixkpt@gmail.com>
2022-06-26 13:50:34 +0300

    Adjusting thumb-image size

--- resources/views/posts/components/featured-posts.blade.php
+++ resources/views/posts/components/featured-posts.blade.php
@@ -21,7 +21,7 @@
                     </div>
                 </a>
             </div>
-            <div class="pl-1 col-8 col-sm-9 col-md-12 col-xl-8 text-left text-xlg-center"><a style="font-size: 16px" href="{{ url('company/'.$item->slug) }}">{{ $item->company_name }}</a></div>
+            <div class="pl-1 col-8 col-sm-9 col-md-12 col-xl-8 text-left text-xlg-center m-auto"><a style="font-size: 16px" href="{{ url('company/'.$item->slug) }}">{{ $item->company_name }}</a></div>
         </div>
         <div class="row my-2">
             <a class="text-dark" href="{{ url('company/'.$item->slug) }}">
